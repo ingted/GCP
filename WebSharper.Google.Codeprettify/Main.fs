1fb8d6a71cfb5889fe98a904bc9866b66c5c7a3d
Gergely Fabian <gergely.fabian@budapest.intellifactory.com>
2017-06-30 17:52:16 +0200

    added multiple language extensions and css extensions

--- WebSharper.Google.Codeprettify/Main.fs
+++ WebSharper.Google.Codeprettify/Main.fs
@@ -17,9 +17,47 @@ module Definition =
     let Assembly =
         Assembly [
             Namespace "WebSharper.Google.Codeprettify.Resources" [
-                Resource "Js" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-ml.js" |> AssemblyWide
-                Resource "Css" "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=sons-of-obsidian&autorun=false"
+                Resource "Apollo" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-apollo.js"
+                Resource "Basic" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-basic.js"
+                Resource "Clj" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-clj.js"
+                Resource "Css" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-css.js"
+                Resource "Dart" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-dart.js"
+                Resource "Erlang" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-erlang.js"
+                Resource "Ex" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-ex.js"
+                Resource "Go" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-go.js"
+                Resource "Hs" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-hs.js"
+                Resource "Lasso" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-lasso.js"
+                Resource "Lisp" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-lisp.js"
+                Resource "Llvm" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-llvm.js"
+                Resource "Logtalk" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-logtalk.js"
+                Resource "Lua" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-lua.js"
+                Resource "Matlab" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-matlab.js"
+                Resource "Ml" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-ml.js"
+                Resource "Mumps" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-mumps.js"
+                Resource "N" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-n.js"
+                Resource "Pascal" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-pascal.js"
+                Resource "Proto" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-proto.js"
+                Resource "R" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-r.js"
+                Resource "Rd" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-rd.js"
+                Resource "Rust" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-rust.js"
+                Resource "Scala" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-scala.js"
+                Resource "Sql" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-sql.js"
+                Resource "Swift" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-swift.js"
+                Resource "Tcl" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-tcl.js"
+                Resource "Tex" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-tex.js"
+                Resource "Vb" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-vb.js"
+                Resource "Vhdl" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-vhdl.js"
+                Resource "Wiki" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-wiki.js"
+                Resource "Xq" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-xq.js"
+                Resource "Yaml" "https://cdn.rawgit.com/google/code-prettify/master/src/lang-yaml.js"
+
+                Resource "Js" "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"
                 |> AssemblyWide
+
+                Resource "DesertCss" "https://cdn.rawgit.com/google/code-prettify/master/styles/desert.css"
+                Resource "DoxyCss" "https://cdn.rawgit.com/google/code-prettify/master/styles/doxy.css"
+                Resource "ObsidianCss" "https://cdn.rawgit.com/google/code-prettify/master/styles/sons-of-obsidian.css"
+                Resource "SunburstCss" "https://cdn.rawgit.com/google/code-prettify/master/styles/sunburst.css"
             ]
             Namespace "WebSharper.Google.Codeprettify" [
                 PR
